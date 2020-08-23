61d15c325d3026abcaa8b2da95a804b6bb36da81
Connor Bailey <connor.baileyyy@gmail.com>
2020-08-23 13:29:36 -0400

    Changed placeholder.jpg with a nicer image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,17 +29,16 @@ function Footer() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
 
+    function handleResize() {
+        setWindowWidth(window.innerWidth);
+    }
+
     useEffect(() => {
-
-        function handleResize() {
-            setWindowWidth(window.innerWidth);
-        }
-
         window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
+        
+        return(
+            window.removeEventListener('resize', handleResize)
+        );
 
     }, []);
 
